43beae0b53e51c26da5ab372c90e3a334c55b236
Katelyn Ge <katelyn.ge@gmail.com>
2018-01-21 18:20:12 -0600

    facebook login
    initial commit

---
+++ Fend App/Fend App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Fend App
+//
+//  Created by Katelyn Ge on 1/21/18.
+//  Copyright © 2018 Fend. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        //super.viewDidLoad()
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
