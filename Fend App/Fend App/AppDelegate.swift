66bf272f088f99dec89243038d9eeb895a55e83f
Katelyn Ge <katelyn.ge@gmail.com>
2018-01-22 14:09:14 -0600

    app delegate connection

--- Fend App/Fend App/AppDelegate.swift
+++ Fend App/Fend App/AppDelegate.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 import FacebookCore
+import FacebookLogin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open: url, options: options)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
