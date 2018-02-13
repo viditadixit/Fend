6da286fc1371ed8b9aabe06af5947c933893d3ca
Sonia Taneja <sonia@Sonias-Mac.local>
2018-02-13 15:12:18 -0800

    config files authorization

--- Fend App/Fend App/ViewController.swift
+++ Fend App/Fend App/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     var dict : [String : AnyObject]!
     var ref: DatabaseReference!
+    var refReports: DatabaseReference!
     
     override func viewDidLoad() {
         //super.viewDidLoad()
@@ -25,12 +26,17 @@ class ViewController: UIViewController {
         view.addSubview(loginButton)
         // Do any additional setup after loading the view, typically from a nib.
         //if the user is already logged in
+        
         if let accessToken = FBSDKAccessToken.current(){
             getFBUserData()
         }
         self.ref = Database.database().reference(fromURL: "fend1-7e1bd.firebaseio.com")
-           }
+        
+    /*
+    refReports = Database.database().reference().child("reports");
     
+ */
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -39,7 +45,7 @@ class ViewController: UIViewController {
     //function is fetching the user data
     func getFBUserData(){
         if((FBSDKAccessToken.current()) != nil){
-            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, email"]).start(completionHandler: { (connection, result, error) -> Void in
                 if (error == nil){
                     self.dict = result as! [String : AnyObject]
                     print(result!)
@@ -49,6 +55,17 @@ class ViewController: UIViewController {
             })
         }
     }
+    /*
+    func addReport(){
+        let key = refReports.childByAutoID().key
+        let report = ["id": key,
+                      "date": Date.text! as String,
+                      "location": LocationText.text! as String,
+                      "description" : DescriptionTextField.text! as String ]
+        refReports.child(key).setValue(report);
+        
+    }
+  */
     
     
 }
